6b8b8f108b9f420d99bba7353a66396f8bfd833b
kafadarova <kafadarova@sendesignal.de>
2019-10-01 16:35:36 +0200

    connect the header + put links inside routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header/>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/delete" exact component={StreamDelete} />
