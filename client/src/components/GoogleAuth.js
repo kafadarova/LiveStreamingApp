3a7bfc5cca3e041c691a8747a0a8a329c0fb4fb0
kafadarova <k.kafadarova@gmail.com>
2019-10-09 16:47:04 +0200

    button refactoring

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
@@ -9,11 +10,39 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         // reference to the auth instance
         this.auth = window.gapi.auth2.getAuthInstance();
+        this.onAuthChange(); // this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        // event listener to check for signin/signout
+        this.auth.isSignedIn.listen(this.onAuthChange);
       })
     });
   }
+  // callback function - so arrow function
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+      if (this.state.isSignedIn === null) {
+        return null;
+      } else  if (this.state.isSignedIn){
+        return (
+          <button className="ui red google button">
+            <i className="google icon" />
+              Sign Out
+          </button>
+        );
+      } else {
+        return (
+          <button className="ui red google button">
+            <i className="google icon" />
+              Sign In with Google
+          </button>
+        );
+      }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
