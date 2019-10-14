fdc1b35b070f6289f2244481b209472542207487
kafadarova <k.kafadarova@gmail.com>
2019-10-14 09:24:06 +0200

    integrate redux dev tool extension

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,11 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
-const INTIAL_STATE = {
+const INITIAL_STATE = {
   isSignedIn: null,
   userId: null
 };
 
-export default (state = INTIAL_STATE, action) => {
+export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
       return { ...state, isSignedIn: true, userId: action.payload };
