a77f958b8910530c61aae2e8ba0a0094f4153a27
kafadarova <k.kafadarova@gmail.com>
2019-10-18 15:39:38 +0200

    Add field components

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    return <input {...formProps.input} /> // take all key-value pairs and add them as prop to the input element
+  }
+
   render() {
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     )
   }
