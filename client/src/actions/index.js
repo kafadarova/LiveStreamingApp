e99d311b5502057eecf71c910c6d768675a94cc0
kafadarova <k.kafadarova@gmail.com>
2019-10-25 16:41:33 +0200

    Render delete/edit button if admin

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -26,8 +26,10 @@ export const signOut = () => {
 
 // action -  create a stream on form submit
 // post request to streams endpoint
-export const createStream = formValues => async dispatch => {
- const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  // get the user id from the auth property in the state object
+  const { userId } =getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
  dispatch({ type: CREATE_STREAM, payload: response.data});
 }
