a2420b507ba2b985f2159acfd176e03e34dfaeb9
kafadarova <k.kafadarova@gmail.com>
2019-10-30 15:33:27 +0100

    Implement programmatic navigation by creating a history object and using Router instead BrowserRouter

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -32,6 +33,8 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
  dispatch({ type: CREATE_STREAM, payload: response.data});
+ // navigate the user to the list of streams after creating one
+ history.push('/');
 }
 
 // get all the records
---
+++ client/src/history.js
@@ -0,0 +1,4 @@
+import { createBrowserHistory } from 'history';
+// create new history object
+
+export default createBrowserHistory();
