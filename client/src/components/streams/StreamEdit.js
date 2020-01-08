4fc39dfed074fe2cf26927e50ce659f284db9c2b
kafadarova <k.kafadarova@gmail.com>
2020-01-08 16:05:36 +0100

    Pick only the values we want to change in the StreamForm

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream, editStream } from "../../actions";
@@ -20,7 +21,7 @@ class StreamEdit extends React.Component {
       <div>
         <h3>Edit a Stream</h3>
         <StreamForm
-        initialValues={this.props.stream}
+        initialValues={_.pick(this.props.stream, 'title', 'description')}
         onSubmit={this.onSubmit} />
       </div>
     );
