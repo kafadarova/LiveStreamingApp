2687f1da789ef14c8d295e7b4fb4fe8cc98a3838
kafadarova <k.kafadarova@gmail.com>
2020-01-09 13:13:29 +0100

    Add a delete a stream functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -63,4 +63,6 @@ export const editStream = (id, formValues) => async dispatch => {
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
   dispatch({type: DELETE_STREAM, payload: id});
+  
+  history.push('/');
 }
